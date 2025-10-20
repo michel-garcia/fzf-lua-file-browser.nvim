7b128857b6d55367a771544c48355135746ac060
Michel Garcia <michelgarxia@gmail.com>
2025-10-19 20:29:56 -0400

    feat: initial rewrite

--- lua/fzf-lua-file-browser/actions.lua
+++ lua/fzf-lua-file-browser/actions.lua
@@ -1,116 +1,126 @@
-local Path = require("fzf-lua-file-browser.path")
+local fzf_actions = require("fzf-lua.actions")
+local fzf_path = require("fzf-lua.path")
 
 local M = {}
 
-local browse = function (opts)
-    local ok, fzf = pcall(require, "fzf-lua")
-    if ok then
-        fzf.file_browser(opts)
+M.open = function(selected, opts)
+    if not next(selected) then
+        return
     end
-end
-
-M.edit_or_browse = function (selected, opts)
-    if next(selected) then
-        local path = Path(opts.cwd, selected[1])
-        if path:is_file() then
-            local filename = path:str()
-            vim.cmd.edit(filename)
-            return
-        end
-        opts.cwd = path:str()
+    local item = selected[1]
+    local browser = require("fzf-lua-file-browser")
+    local file = browser.state.files[item] or nil
+    if not file then
+        return
     end
-    browse(opts)
-end
-
-M.go_to_parent = function (_, opts)
-    local path = Path(opts.cwd)
-    local parent = path:parent()
-    opts.cwd = parent:str()
-    local basename = path:basename()
-    opts.selection = basename
-    browse(opts)
-end
-
-M.go_to_cwd = function (_, opts)
-    opts.cwd = Path.cwd():str()
-    browse(opts)
-end
-
-M.toggle_hidden = function (_, opts)
-    opts.hidden = not opts.hidden
-    browse(opts)
-end
-
-M.go_to_home = function (_, opts)
-    opts.cwd = Path.home():str()
-    browse(opts)
-end
-
-M.create = function (_, opts)
-    local ok, input = pcall(vim.fn.input, "Enter filename: ")
-    if ok and input ~= "" then
-        local path = Path(opts.cwd, input)
-        if path:exists() then
-            local message = string.format("%s already exists", path)
-            vim.notify(message, "error")
-        else
-            ok = path:create()
-            if ok then
-                opts.selection = path:basename()
-            else
-                local message = string.format(
-                    "Failed to create %s", input
-                )
-                vim.notify(message, "error")
-            end
-        end
+    if not file.is_dir then
+        fzf_actions.file_edit({ file.path }, opts)
+        return
     end
-    browse(opts)
+    browser.state.cwd = file.path
+    browser.browse(vim.tbl_extend("force", opts, { cwd = browser.state.cwd }))
 end
 
-M.rename = function (selected, opts)
-    if next(selected) then
-        local path = Path(opts.cwd, selected[1])
-        local prompt = string.format("Move %s to: ", path)
-        local ok, input = pcall(vim.fn.input, prompt, path:str())
-        if ok and input ~= "" and input ~= path:str() then
-            if Path(input):exists() then
-                local message = string.format(
-                    "%s already exists", input
-                )
-                vim.notify(message, "error")
-            else
-                ok = path:rename(input)
-                if ok then
-                    opts.selection = Path(input):basename()
+M.parent = function(_, opts)
+    local browser = require("fzf-lua-file-browser")
+    browser.state.active = vim.fn.fnamemodify(vim.fs.normalize(browser.state.cwd), ":t")
+    browser.state.cwd = fzf_path.parent(browser.state.cwd)
+    browser.browse(vim.tbl_extend("force", opts, { cwd = browser.state.cwd }))
+end
+
+M.cwd = function(_, opts)
+    local browser = require("fzf-lua-file-browser")
+    browser.state.cwd = vim.loop.cwd()
+    browser.browse(vim.tbl_extend("force", opts, { cwd = browser.state.cwd }))
+end
+
+M.home = function(_, opts)
+    local browser = require("fzf-lua-file-browser")
+    browser.state.cwd = vim.loop.os_homedir()
+    browser.browse(vim.tbl_extend("force", opts, { cwd = browser.state.cwd }))
+end
+
+M.toggle_hidden = function(_, opts)
+    local browser = require("fzf-lua-file-browser")
+    browser.state.hidden = not browser.state.hidden
+    browser.browse(vim.deepcopy(opts))
+end
+
+M.create = function(_, opts)
+    vim.ui.input({
+        prompt = "New name: ",
+    }, function(name)
+        local browser = require("fzf-lua-file-browser")
+        if name then
+            local cwd = opts.cwd or browser.state.cwd
+            local path = fzf_path.join({ cwd, name })
+            local stat = vim.loop.fs_stat(path) or {}
+            if vim.tbl_isempty(stat) then
+                if name:sub(-1) == "/" then
+                    vim.loop.fs_mkdir(path, 493)
                 else
-                    local message = string.format(
-                        "Failed to move %s", path
-                    )
-                    vim.notify(message, "error")
+                    local handle = vim.loop.fs_open(path, "w", 420)
+                    if handle then
+                        vim.loop.fs_close(handle)
+                    end
                 end
+            else
+                vim.notify("File already exists.", vim.log.levels.ERROR)
             end
+            browser.state.active = name
         end
-    end
-    browse(opts)
+        browser.browse(opts)
+    end)
 end
 
-M.delete = function (selected, opts)
-    if next(selected) then
-        local path = Path(opts.cwd, selected[1])
-        local prompt = string.format("Delete %s? [y/n] ", path)
-        local ok, input = pcall(vim.fn.input, prompt)
-        if ok and input == "y" then
-            ok = path:delete()
-            if not ok then
-                local message = string.format(
-                    "Failed to delete %s", path
-                )
-                vim.notify(message, "error")
-            end
-        end
+M.rename = function(selected, opts)
+    if not next(selected) then
+        return
     end
-    browse(opts)
+    local item = selected[1]
+    local file = M.state.files[item] or nil
+    if not file then
+        return
+    end
+    vim.ui.input({
+        prompt = "New name: ",
+        default = file.name,
+    }, function(name)
+        local browser = require("fzf-lua-file-browser")
+        if name then
+            local cwd = opts.cwd or browser.state.cwd
+            local path = fzf_path.join({ cwd, name })
+            local stat = vim.loop.fs_stat(path) or {}
+            if vim.tbl_isempty(stat) then
+                vim.loop.fs_rename(file.path, path)
+            else
+                vim.notify("File already exists.", vim.log.levels.ERROR)
+            end
+            browser.state.active = name
+        end
+        browser.browse(opts)
+    end)
+end
+
+M.delete = function(selected, opts)
+    if not next(selected) then
+        return
+    end
+    local item = selected[1]
+    local browser = require("fzf-lua-file-browser")
+    local file = browser.state.files[item]
+    vim.ui.input({
+        prompt = string.format("Delete %s? [y/n]", file.name),
+    }, function(input)
+        if input == "y" then
+            if file.is_dir then
+                vim.loop.fs_rmdir(file.path)
+            else
+                vim.loop.fs_unlink(file.path)
+            end
+            browser.browse(opts)
+        end
+    end)
 end
 
 return M
--- lua/fzf-lua-file-browser/init.lua
+++ lua/fzf-lua-file-browser/init.lua
@@ -1,91 +1,198 @@
-local Path = require("fzf-lua-file-browser.path")
+local fzf = require("fzf-lua")
+local fzf_path = require("fzf-lua.path")
+local fzf_utils = require("fzf-lua.utils")
+
 local actions = require("fzf-lua-file-browser.actions")
-local utils = require("fzf-lua-file-browser.utils")
+
+local hijack_netrw = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+            pcall(vim.api.nvim_clear_autocmds, {
+                group = "FileExplorer",
+            })
+        end,
+        once = true,
+    })
+    vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+            vim.schedule(function()
+                local bufname = vim.api.nvim_buf_get_name(0)
+                if vim.fn.isdirectory(bufname) == 0 then
+                    return
+                end
+                if bufname == vim.g.netrw_bufname then
+                    return
+                end
+                vim.g.netrw_bufname = bufname
+                vim.api.nvim_set_option_value("bufhidden", "wipe", {
+                    buf = 0,
+                })
+                local browser = require("fzf-lua-file-browser")
+                browser.browse()
+            end)
+        end,
+        group = vim.api.nvim_create_augroup("FzfLuaFileBrowser", {
+            clear = true,
+        }),
+    })
+end
+
+local get_files = function(path)
+    local handle = vim.loop.fs_scandir(path)
+    if not handle then
+        return {}
+    end
+    local files = {}
+    while true do
+        local name, typ = vim.loop.fs_scandir_next(handle)
+        if not name then
+            break
+        end
+        if typ == "link" then
+            local stat = vim.loop.fs_stat(name) or {}
+            typ = stat and stat.type or "file"
+        end
+        table.insert(files, {
+            name = name,
+            type = typ,
+            is_dir = typ == "directory",
+            is_hidden = name:sub(1, 1) == ".",
+            path = fzf_path.join({ path, name }),
+        })
+    end
+    return files
+end
 
 local M = {}
 
-local loaded = false
-
 M.opts = {
     actions = {
-        ["default"] = actions.edit_or_browse,
-        ["ctrl-g"] = actions.go_to_parent,
-        ["ctrl-w"] = actions.go_to_cwd,
-        ["ctrl-h"] = actions.toggle_hidden,
-        ["ctrl-e"] = actions.go_to_home,
+        ["default"] = actions.open,
+        ["ctrl-g"] = actions.parent,
+        ["ctrl-w"] = actions.cwd,
+        ["ctrl-e"] = actions.home,
+        ["ctrl-t"] = actions.toggle_hidden,
         ["ctrl-a"] = actions.create,
         ["ctrl-r"] = actions.rename,
-        ["ctrl-x"] = actions.delete
+        ["ctrl-d"] = actions.delete,
     },
-    cwd = nil,
-    fzf_opts = {},
-    group_directories_first = true,
-    hidden = false,
-    natural_sort = true,
-    prompt = "File Browser> ",
-    reverse = false,
+    color_icons = true,
+    dir_icon = "󰉋",
+    dir_icon_hl = "Directory",
+    file_icons = true,
+    hidden = true,
+    hijack_netrw = false,
     show_cwd_header = true,
-    sort = "name"
 }
 
-M.setup = function (opts)
-    if loaded then
-        return
+M.state = {
+    cwd = nil,
+    files = {},
+    hidden = true,
+    active = nil,
+}
+
+M.make_item = function(file)
+    local _, icons = pcall(require, "nvim-web-devicons")
+    local name = file.is_dir and string.format("%s/", file.name) or file.name
+    local icon, color = nil, nil
+    if M.opts.file_icons and icons then
+        if file.is_dir then
+            icon = M.opts.dir_icon
+            if M.opts.dir_icon_hl then
+                local highlight = vim.api.nvim_get_hl(0, {
+                    name = M.opts.dir_icon_hl,
+                    link = false,
+                })
+                if highlight and highlight.fg then
+                    color = string.format("#%06x", highlight.fg)
+                end
+            end
+        else
+            icon, color = icons.get_icon_color(file.name)
+            if not icon then
+                icon, color = icons.get_icon_color("txt")
+            end
+        end
     end
-    local ok, fzf = pcall(require, "fzf-lua")
-    if not ok then
-        return
+    local key = table.concat(
+        vim.tbl_filter(function(value)
+            return value
+        end, { icon, name }),
+        " "
+    )
+    if M.opts.color_icons and color then
+        icon = fzf_utils.ansi_from_rgb(color, icon)
     end
-    M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
-    if fzf.file_browser then
-        vim.notify("Existing FzfLua file browser extension detected", "warn")
-        return
-    end
-    fzf.file_browser = M.browse
-    loaded = true
+    local item = table.concat(
+        vim.tbl_filter(function(value)
+            return value
+        end, { icon, name }),
+        " "
+    )
+    return item, key
 end
 
-M.browse = function (opts)
-    opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+M.browse = function(opts)
+    opts = opts or {}
+    M.state.cwd = opts.cwd or vim.fn.expand("%:p:h") or vim.loop.cwd()
+    M.state.files = {}
     if not opts.cwd then
-        opts.selection = vim.fn.expand("%:t:h")
+        M.state.active = vim.fn.expand("%:t:h")
     end
-    opts.cwd = opts.cwd or vim.fn.expand("%:p:h") or vim.loop.cwd()
-    if opts.show_cwd_header then
-        local header = vim.fn.fnamemodify(opts.cwd, ":~")
-        opts.fzf_opts["--header"] = header
+    local files = get_files(M.state.cwd)
+    if not M.state.hidden then
+        files = vim.tbl_filter(function(file)
+            return not file.is_hidden
+        end, files)
     end
-    opts.files = Path(opts.cwd):files()
-    if not opts.hidden then
-        opts.files = vim.tbl_filter(function (file)
-            return not file:is_hidden()
-        end, opts.files)
+    table.sort(files, function(a, b)
+        if a.type == b.type then
+            return a.name:lower() < b.name:lower()
+        end
+        return a.is_dir
+    end)
+    local fzf_args = {
+        "--sync",
+        "--bind change:first",
+    }
+    if M.opts.show_cwd_header then
+        local path = vim.fn.pathshorten(M.state.cwd)
+        local arg = string.format("--header=%s", path)
+        table.insert(fzf_args, arg)
     end
-    utils.sort(opts.files, opts)
-    if opts.reverse then
-        opts.files = utils.reverse(opts.files)
-    end
-    if opts.selection then
-        local index = vim.fn.indexof(opts.files, function (_, file)
-            return Path(file.path):basename() == opts.selection
-        end)
-        if index > 0 then
-            local steps = {}
-            for _ = 1, index do
-                table.insert(steps, "down")
-            end
-            opts.fzf_args = string.format(
-                "--sync --bind start:%s", table.concat(steps, "+")
-            )
+    local items = {}
+    for i, file in ipairs(files) do
+        local item, key = M.make_item(file)
+        table.insert(items, item)
+        M.state.files[key] = file
+        if file.name == M.state.active then
+            local arg = string.format("--bind start:pos\\(%s\\)", i)
+            table.insert(fzf_args, arg)
         end
     end
-    local core = require("fzf-lua.core")
-    core.fzf_exec(function (callback)
-        for _, file in ipairs(opts.files) do
-            callback(file:basename())
+    fzf.fzf_exec(function(callback)
+        for _, item in ipairs(items) do
+            callback(item)
         end
         callback()
-    end, opts)
+    end, {
+        actions = M.opts.actions,
+        cwd = M.state.cwd,
+        fzf_args = table.concat(fzf_args, " "),
+        winopts = {
+            title = " File Browser ",
+        },
+    })
+end
+
+M.setup = function(opts)
+    M.opts = vim.tbl_extend("force", M.opts, opts or {})
+    M.state.hidden = M.opts.hidden
+    if M.opts.hijack_netrw then
+        hijack_netrw()
+    end
+    fzf.file_browser = M.browse
 end
 
 return M
