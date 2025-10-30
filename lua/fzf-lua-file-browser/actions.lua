d3411a1753448cc4de1aedafe05175faa9b873dd
Michel Garcia <michelgarxia@gmail.com>
2025-10-30 16:47:55 -0400

    feat: reimplement blocking actions as custom functions

--- lua/fzf-lua-file-browser/actions.lua
+++ lua/fzf-lua-file-browser/actions.lua
@@ -67,85 +67,101 @@ M.toggle_hidden = function(_, opts)
     browser.browse(opts)
 end
 
-M.create = function(_, opts)
-    local browser = require("fzf-lua-file-browser")
-    local cwd = opts.cwd or browser.state.cwd
-    vim.ui.input({
-        prompt = "New path: ",
-        default = fzf_path.add_trailing(cwd),
-    }, function(path)
-        if path then
+M.create = {
+    fn = function(_, opts)
+        local browser = require("fzf-lua-file-browser")
+        local cwd = opts.cwd or browser.state.cwd
+        vim.ui.input({
+            prompt = "New path: ",
+            default = fzf_path.add_trailing(cwd),
+        }, function(path)
+            if not path then
+                return
+            end
             local stat = vim.loop.fs_stat(path) or {}
-            if vim.tbl_isempty(stat) then
-                local parent = fzf_path.parent(path)
-                vim.fn.mkdir(parent, "p")
-                if path:sub(-1) == "/" then
-                    vim.loop.fs_mkdir(path, 493)
-                else
-                    local handle = vim.loop.fs_open(path, "w", 420)
-                    if handle then
-                        vim.loop.fs_close(handle)
-                    end
-                end
-            else
+            if not vim.tbl_isempty(stat) then
                 vim.notify("File already exists.", vim.log.levels.ERROR)
+                return
             end
-            browser.state.active = fzf_path.basename(path)
-        end
-        browser.browse(opts)
-    end)
-end
-
-M.rename = function(selected, opts)
-    if not next(selected) then
-        return
-    end
-    local browser = require("fzf-lua-file-browser")
-    local key = selected[1]
-    local file = browser.state.files[key] or nil
-    if not file then
-        return
-    end
-    vim.ui.input({
-        prompt = "New path: ",
-        default = file.path,
-    }, function(path)
-        if path then
-            if path ~= file.path then
-                local stat = vim.loop.fs_stat(path) or {}
-                if vim.tbl_isempty(stat) then
-                    local parent = fzf_path.parent(path)
-                    vim.fn.mkdir(parent, "p")
-                    vim.loop.fs_rename(file.path, path)
-                else
-                    vim.notify("File already exists.", vim.log.levels.ERROR)
+            local parent = fzf_path.parent(path)
+            vim.fn.mkdir(parent, "p")
+            if path:sub(-1) == "/" then
+                vim.loop.fs_mkdir(path, 493)
+            else
+                local handle = vim.loop.fs_open(path, "w", 420)
+                if handle then
+                    vim.loop.fs_close(handle)
                 end
             end
             browser.state.active = fzf_path.basename(path)
-        end
-        browser.browse(opts)
-    end)
-end
+            browser.browse(opts)
+        end)
+    end,
+    reload = true,
+}
 
-M.delete = function(selected, opts)
-    if not next(selected) then
-        return
-    end
-    local key = selected[1]
-    local browser = require("fzf-lua-file-browser")
-    local file = browser.state.files[key]
-    vim.ui.input({
-        prompt = string.format("Delete %s? [y/n]", file.name),
-    }, function(input)
-        if input == "y" then
+M.rename = {
+    fn = function(selected, opts)
+        if not next(selected) then
+            return
+        end
+        local key = selected[1]
+        local browser = require("fzf-lua-file-browser")
+        local file = browser.state.files[key] or nil
+        if not file then
+            return
+        end
+        vim.ui.input({
+            prompt = "New path: ",
+            default = file.path,
+        }, function(path)
+            if not path then
+                return
+            end
+            if path == file.path then
+                return
+            end
+            local stat = vim.loop.fs_stat(path) or {}
+            if not vim.tbl_isempty(stat) then
+                vim.notify("File already exists.", vim.log.levels.ERROR)
+                return
+            end
+            local parent = fzf_path.parent(path)
+            vim.fn.mkdir(parent, "p")
+            vim.loop.fs_rename(file.path, path)
+            browser.state.active = fzf_path.basename(path)
+        end)
+        browser.browse(opts)
+    end,
+    reload = true,
+}
+
+M.delete = {
+    fn = function(selected, opts)
+        if not next(selected) then
+            return
+        end
+        local key = selected[1]
+        local browser = require("fzf-lua-file-browser")
+        local file = browser.state.files[key] or nil
+        if not file then
+            return
+        end
+        vim.ui.input({
+            prompt = string.format("Delete %s? [y/n] ", file.name),
+        }, function(input)
+            if input ~= "y" then
+                return
+            end
             if file.is_dir then
                 rmdir(file.path)
             else
                 vim.loop.fs_unlink(file.path)
             end
             browser.browse(opts)
-        end
-    end)
-end
+        end)
+    end,
+    reload = true,
+}
 
 return M
